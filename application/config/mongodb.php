02a11911bf9de677a228da660ec51069478915c0
chairuddinsyah <chairuddinsyah@10.100.8.149>
2016-04-06 18:48:45 +0700

    Initial Commit

--- application/config/mongodb.php
+++ application/config/mongodb.php
@@ -5,7 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['username'] = 'billionaire';
 $config['password'] = '4legrium';
-$config['hostname'] = '10.100.8.152';
+$config['hostname'] = 'localhost';
 $config['database'] = 'billionaire_prod';
 $config['database_dev'] = 'billionaire_dev';
 
