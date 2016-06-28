0cdc82ebd4b0704d7fe5886c6a3053b8aca5ee74
chairuddinsyah <chairuddinsyah@10.100.8.149>
2016-06-28 16:18:52 +0700

    Update Commit

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -15,5 +15,9 @@ class Home extends CI_Controller {
     public function index() {
         $this->load->view('home_view');
     }
+    
+    public function info() {
+        phpinfo();
+    }
 
 }
